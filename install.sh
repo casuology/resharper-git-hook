8ad59a9b27f924f24db420411aa10ad182f3fc17
Dante Sparrås <55974949+dante-sparras@users.noreply.github.com>
2024-09-27 21:57:18 +0200

    update install.sh

--- install.sh
+++ install.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-localGitDir="./.git"
+localPreCommitFilePath="./.git/hooks/pre-commit"
 localDotnetManifestFilePath="./.config/dotnet-tools.json"
-localGitHooksDir="./.githooks"
-localPreCommitFilePath="$localGitHooksDir/pre-commit"
 preCommitFileUrl="https://raw.githubusercontent.com/casuology/resharper-git-hook/main/pre-commit.sh"
 
 # If a .git folder does not exist, exit
-if ! [ -d "$localGitDir" ]; then
+if ! [ -d "./.git" ]; then
     echo "Error: No .git folder found in the current directory"
     exit 1
 fi
@@ -26,16 +24,11 @@ if [ $(("$dotnetMajorVersion")) -lt 3 ]; then
     exit 1
 fi
 
-# If the .githooks folder does not exist, create it
-if ! [ -d "$localGitHooksDir" ]; then
-    echo "Info: Creating .githooks folder"
-    mkdir "$localGitHooksDir"
-fi
-
 # If the pre-commit file does not exist, fetch it from the repository
 if ! [ -f "$localPreCommitFilePath" ]; then
     echo "Info: Fetching pre-commit file from repository"
     curl -o "$localPreCommitFilePath" "$preCommitFileUrl"
+    chmod +x "$localPreCommitFilePath"
 fi
 
 # If a dotnet tool manifest file does not exist, create it
