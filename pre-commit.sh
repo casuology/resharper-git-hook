150837f9b43412de9146f86019d0bd775ce6a389
Dante Sparrås <55974949+dante-sparras@users.noreply.github.com>
2024-09-27 21:57:09 +0200

    update pre-commit.sh

--- pre-commit.sh
+++ pre-commit.sh
@@ -1,1 +1,21 @@
 #!/bin/bash
+
+stagedFilesExcludingDeleted=$(git diff --cached --name-only --diff-filter=d)
+
+echo "Running code cleanup on: $stagedFilesExcludingDeleted"
+
+# If no files are staged, exit
+if [ -z "$stagedFilesExcludingDeleted" ]; then
+    echo "No files to perform code cleanup on"
+    exit 0
+fi
+
+# Run code cleanup on the staged files
+dotnet jb cleanupcode $stagedFilesExcludingDeleted
+echo "Code cleanup completed successfully ✅"
+
+# Restage the files
+git add $stagedFilesExcludingDeleted
+echo "Modified files staged ✅"
+
+exit 0
